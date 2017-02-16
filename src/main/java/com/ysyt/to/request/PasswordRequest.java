86f16916cf8eae3397be0dd33d4b667e433ae650
flipticket <flipticket@gmail.com>
2017-02-16 22:11:26 +0530

    password request @rajesh

---
+++ src/main/java/com/ysyt/to/request/PasswordRequest.java
@@ -0,0 +1,24 @@
+package com.ysyt.to.request;
+
+public class PasswordRequest {
+
+	private String newPassword;
+	private String oldPassword;
+
+	public String getNewPassword() {
+		return newPassword;
+	}
+
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
+	}
+
+	public String getOldPassword() {
+		return oldPassword;
+	}
+
+	public void setOldPassword(String oldPassword) {
+		this.oldPassword = oldPassword;
+	}
+
+}
