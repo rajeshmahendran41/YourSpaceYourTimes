0a608134bfeafb4b3ce72bbddaf3755fb64deab7
flipticket <flipticket@gmail.com>
2017-02-26 19:32:10 +0530

    Added S3 AWS Integration with YSYT @rajesh

---
+++ src/main/java/com/aws/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.aws.controllers;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "")
+	public String home() {
+		return "index";
+	}
+}
