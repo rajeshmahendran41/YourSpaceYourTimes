34e9e9727feef23a824294b7c298bc7625041c4b
flipticket <flipticket@gmail.com>
2017-03-14 12:33:31 +0530

    Update CORSFilter.java

--- src/main/java/com/config/CORSFilter.java
+++ src/main/java/com/config/CORSFilter.java
@@ -32,12 +32,12 @@ public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
     response.setHeader("Access-Control-Allow-Credentials", "true");
     response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
     response.setHeader("Access-Control-Max-Age", "3600");
-    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me,X_CSRF_TOKEN");
 
     chain.doFilter(req, res);
 }
 
-@Override
+@Override 
 public void init(FilterConfig filterConfig) {
 }
 
